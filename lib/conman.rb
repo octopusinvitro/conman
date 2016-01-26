8110da547197a919abb232aa36108680ac1169dd
me-stevens <me-stevens@users.noreply.github.com>
2016-01-26 14:48:56 +0000

    Refactor and add the rest of the contact fields.

--- lib/conman.rb
+++ lib/conman.rb
@@ -8,20 +8,44 @@ class Conman
   end
 
   def add_contact
-  	console.print("Contact name: ")
-  	name = console.read.chomp
-
-  	console.print("Contact address: ")
-  	address = console.read.chomp
-
-    console.print("Contact phone: ")
-    phone = console.read.chomp
+  	name    = add_name
+  	address = add_address
+  	phone   = add_phone
+    email   = add_email
+    notes   = add_notes
 
   	contact = {}
   	contact[:name]    = name
   	contact[:address] = address
   	contact[:phone]   = phone
+  	contact[:email]   = email
+  	contact[:notes]   = notes
   	contact_list << contact
+  end
+
+  def add_name
+  	console.print("Contact name: ")
+  	console.read.chomp
+  end
+
+  def add_address
+	console.print("Contact address: ")
+  	console.read.chomp
+  end
+
+  def add_phone
+  	console.print("Contact phone: ")
+    console.read.chomp
+  end
+
+  def add_email
+  	console.print("Contact email: ")
+    console.read.chomp
+  end
+
+  def add_notes
+  	console.print("Contact notes")
+  	console.read.chomp
   end
 
   def contact_by_id(contact_id)
--- spec/conman_spec.rb
+++ spec/conman_spec.rb
@@ -2,15 +2,33 @@ require 'conman'
 
 describe Conman do
 
-  it "saves the name introduced by the user" do
-    input  = StringIO.new("name\n")
-    output = StringIO.new
+  let (:input)   {StringIO.new("name\naddress\n123456\nemail@mail.com\nnotes")}
+  let (:output)  {StringIO.new}
+  let (:console) {Console.new(input, output)}
+  let (:conman)  {Conman.new(console)}
 
-    console = Console.new(input, output)
-    conman  = Conman.new(console)
-    
+  it "saves the name introduced by the user" do
     conman.add_contact
     expect(conman.contact_by_id(0)[:name]).to eq("name")
   end
 
+  it "saves the address introduced by the user" do
+    conman.add_contact
+    expect(conman.contact_by_id(0)[:address]).to eq("address")
+  end
+
+  it "saves the phone introduced by the user" do
+  	conman.add_contact
+  	expect(conman.contact_by_id(0)[:phone]).to eq("123456")
+  end
+
+  it "saves the email introduced by the user" do
+  	conman.add_contact
+  	expect(conman.contact_by_id(0)[:email]).to eq("email@mail.com")
+  end
+
+  it "saves the notes introduced by the user" do
+  	conman.add_contact
+  	expect(conman.contact_by_id(0)[:notes]).to eq("notes")
+  end
 end
