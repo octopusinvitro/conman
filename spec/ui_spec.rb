33d1b8e8ccedefa86ae5b18213cd7a35052fd34c
me-stevens <me-stevens@users.noreply.github.com>
2016-01-26 21:25:20 +0000

    Refactor the ui tests.

--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -3,35 +3,27 @@ require 'ui'
 
 describe UI do
 
+  let(:input)  {StringIO.new}
+  let(:output) {StringIO.new}
+  let(:ui)     {UI.new(Console.new(input, output))}
+
   it "reads an answer" do
-    input   = StringIO.new("y")
-    output  = StringIO.new
-    ui      = UI.new(Console.new(input, output))
-
-    answer  = ui.ask_for_another
-
+    input.string = "y"
+    answer       = ui.ask_for_another
     expect(output.string.chomp).to eq(UI::ADD_ANOTHER)
     expect(answer).to eq("y")
   end
 
   it "reads a name" do
-    input   = StringIO.new("name")
-    output  = StringIO.new
-    ui      = UI.new(Console.new(input, output))
-
-    name  = ui.ask_for_name
-
+    input.string = "name"
+    name         = ui.ask_for_name
     expect(output.string.chomp).to eq(UI::NAME)
     expect(name).to eq("name")
   end
 
   it "reads an address" do
-    input   = StringIO.new("address")
-    output  = StringIO.new
-    ui      = UI.new(Console.new(input, output))
-
-    address = ui.ask_for_address
-
+    input.string = "address"
+    address      = ui.ask_for_address
     expect(output.string.chomp).to eq(UI::ADDRESS)
     expect(address).to eq("address")
   end
