5f28a855a5b546f18b60735611e2abbe58e75079
me-stevens <me-stevens@users.noreply.github.com>
2016-01-26 17:33:40 +0000

    Rename some methods.

--- spec/conman_spec.rb
+++ spec/conman_spec.rb
@@ -2,33 +2,34 @@ require 'conman'
 
 describe Conman do
 
-  let (:input)   {StringIO.new("name\naddress\n123456\nemail@mail.com\nnotes")}
+  let (:input)   {StringIO.new("name\naddress\n123456\nemail@mail.com\nnotes\n")}
   let (:output)  {StringIO.new}
   let (:console) {Console.new(input, output)}
   let (:conman)  {Conman.new(console)}
 
   it "saves the name introduced by the user" do
     conman.add_contact
-    expect(conman.contact_by_id(0)[:name]).to eq("name")
+    expect(conman.contact_of_id(0)[:name]).to eq("name")
   end
 
   it "saves the address introduced by the user" do
     conman.add_contact
-    expect(conman.contact_by_id(0)[:address]).to eq("address")
+    expect(conman.contact_of_id(0)[:address]).to eq("address")
   end
 
   it "saves the phone introduced by the user" do
-  	conman.add_contact
-  	expect(conman.contact_by_id(0)[:phone]).to eq("123456")
+    conman.add_contact
+    expect(conman.contact_of_id(0)[:phone]).to eq("123456")
   end
 
   it "saves the email introduced by the user" do
-  	conman.add_contact
-  	expect(conman.contact_by_id(0)[:email]).to eq("email@mail.com")
+    conman.add_contact
+    expect(conman.contact_of_id(0)[:email]).to eq("email@mail.com")
   end
 
   it "saves the notes introduced by the user" do
-  	conman.add_contact
-  	expect(conman.contact_by_id(0)[:notes]).to eq("notes")
+    conman.add_contact
+    expect(conman.contact_of_id(0)[:notes]).to eq("notes")
   end
+
 end
