253247b406f83bd3c3a300230fb34e9af36c5633
me-stevens <me-stevens@users.noreply.github.com>
2016-01-28 08:20:31 +0000

    Rename the ui fake to just ui.

--- spec/conman_spec.rb
+++ spec/conman_spec.rb
@@ -3,58 +3,58 @@ require 'ui'
 
 describe Conman do
 
-  let (:ui_fake) {instance_double(UI).as_null_object}
-  let (:conman)  {Conman.new(ui_fake)}
+  let (:ui) {instance_double(UI).as_null_object}
+  let (:conman)  {Conman.new(ui)}
 
   it "saves the name introduced by the user" do
-    allow(ui_fake).to receive(:ask_for_name).and_return("name")
+    allow(ui).to receive(:ask_for_name).and_return("name")
     conman.add_contact
     expect_field(:name, "name")
   end
 
   it "saves the address introduced by the user" do
-    allow(ui_fake).to receive(:ask_for_address).and_return("address")
+    allow(ui).to receive(:ask_for_address).and_return("address")
     conman.add_contact
     expect_field(:address, "address")
   end
 
   it "saves the phone introduced by the user" do
-    allow(ui_fake).to receive(:ask_for_phone).and_return("123456")
+    allow(ui).to receive(:ask_for_phone).and_return("123456")
     conman.add_contact
     expect_field(:phone, "123456")
   end
 
   it "saves the email introduced by the user" do
-    allow(ui_fake).to receive(:ask_for_email).and_return("email@mail.com")
+    allow(ui).to receive(:ask_for_email).and_return("email@mail.com")
     conman.add_contact
     expect_field(:email, "email@mail.com")
   end
 
   it "saves the notes introduced by the user" do
-    allow(ui_fake).to receive(:ask_for_notes).and_return("notes")
+    allow(ui).to receive(:ask_for_notes).and_return("notes")
     conman.add_contact
     expect_field(:notes, "notes")
   end
 
   it "adds two contacts through the main loop" do
-    allow(ui_fake).to receive(:ask_for_another).and_return(true, false)
-    allow(ui_fake).to receive(:display) { "" }
+    allow(ui).to receive(:ask_for_another).and_return(true, false)
+    allow(ui).to receive(:display) { "" }
     conman.run
     expect(conman.total_contacts).to eq(2)
   end
 
   it "prints contact after adding it" do
-    allow(ui_fake).to receive(:ask_for_another).and_return(false)
-    allow(ui_fake).to receive(:display) { "" }
+    allow(ui).to receive(:ask_for_another).and_return(false)
+    allow(ui).to receive(:display) { "" }
     conman.run
-    expect(ui_fake).to have_received(:display).once
+    expect(ui).to have_received(:display).once
   end
 
   it "prints all contacts after finished" do
-    allow(ui_fake).to receive(:ask_for_another).and_return(true, false)
-    allow(ui_fake).to receive(:display) { "" }
+    allow(ui).to receive(:ask_for_another).and_return(true, false)
+    allow(ui).to receive(:display) { "" }
     conman.run
-    expect(ui_fake).to have_received(:display_all).once
+    expect(ui).to have_received(:display_all).once
   end
 
   def expect_field(key, value)
