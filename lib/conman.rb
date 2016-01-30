bd0daa22be097cc96e84a62ad1ecaed354c23d20
private <user@example.com>
2016-01-30 20:03:42 +0000

    Extract a class from Conman to search for a contact.

--- lib/conman.rb
+++ lib/conman.rb
@@ -1,9 +1,9 @@
 class Conman
 
-  def initialize(ui, db, creator)
-    @ui       = ui
-    @contacts = db
-    @creator  = creator
+  def initialize(ui, creator, finder)
+    @ui      = ui
+    @creator = creator
+    @finder  = finder
   end
 
   def run
@@ -27,22 +27,16 @@ class Conman
     end
   end
 
-  def search_contacts
-    loop do
-      ui.display_all(search_contact(ui.ask_for_term))
-      break unless ui.ask_search_again
-    end
-  end
-
-  def search_contact(term)
-    contacts.all.select{ |contact| contact.any? {|key, val| val.include?(term)} }
-  end
-
   private
 
-  attr_reader :ui, :contacts, :creator
+  attr_reader :ui, :creator, :finder
 
   def add_contacts
     creator.add_contacts
   end
+
+  def search_contacts
+    finder.search_contacts
+  end
+
 end
