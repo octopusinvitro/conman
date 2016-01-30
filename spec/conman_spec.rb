0b63efefec663823520e9eead7849001c98a9200
private <user@example.com>
2016-01-30 20:39:37 +0000

    Add an option in the menu to list contacts.

--- spec/conman_spec.rb
+++ spec/conman_spec.rb
@@ -7,7 +7,7 @@ describe Conman do
 
   let (:ui)     {instance_double(UI).as_null_object}
   let (:db)     {DB.new}
-  let (:conman) {Conman.new(ui, Creator.new(ui, db), Finder.new(ui, db))}
+  let (:conman) {Conman.new(ui, db)}
 
   before :each do
     allow(ui).to receive(:ask_for_another).and_return(false)
@@ -15,19 +15,25 @@ describe Conman do
   end
 
   it "displays menu once when program runs" do
-    allow(ui).to receive(:ask_menu_option).and_return(3)
+    allow(ui).to receive(:ask_menu_option).and_return(4)
     conman.run
     expect(ui).to have_received(:ask_menu_option).once
   end
 
-  it "calls add contacts if user selects 1" do
-    allow(ui).to receive(:ask_menu_option).and_return(1, 3)
+  it "prints all contacts if user selects to list all" do
+    allow(ui).to receive(:ask_menu_option).and_return(1, 4)
+    conman.run
+    expect(ui).to have_received(:display_all).once
+  end
+
+  it "calls add contacts if user selects to add a contact" do
+    allow(ui).to receive(:ask_menu_option).and_return(2, 4)
     conman.run
     expect(ui).to have_received(:ask_for_name).once
   end
 
-  it "calls search contacts if user selects 2" do
-    allow(ui).to receive(:ask_menu_option).and_return(2, 3)
+  it "calls search contacts if user selects to search for a contact" do
+    allow(ui).to receive(:ask_menu_option).and_return(3, 4)
     allow(ui).to receive(:ask_search_again).and_return(false)
     conman.run
     expect(ui).to have_received(:ask_for_term).once
