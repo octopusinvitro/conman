c496b22a0b8e13197703b0cc9f3c4dce8009da01
me-stevens <me-stevens@users.noreply.github.com>
2016-02-02 23:31:01 +0000

    Add a reader class to read files from filesystem.

---
+++ lib/reader.rb
@@ -0,0 +1,25 @@
+require 'json'
+
+class Reader
+
+  def initialize(file)
+    @file = file
+  end
+
+  def read_contacts
+    parse(read)
+  end
+
+  private
+
+  attr_reader :file
+
+  def read
+    file.read
+  end
+
+  def parse(contacts)
+    JSON.parse(contacts)
+  end
+
+end
