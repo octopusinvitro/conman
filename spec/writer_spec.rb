c37e78427266e375b857a2362d24dea1dcca75ef
private <user@example.com>
2016-02-03 02:05:30 +0000

    Add a writer class to write the contacts to a file.

---
+++ spec/writer_spec.rb
@@ -0,0 +1,20 @@
+require 'writer'
+
+describe Writer do
+
+  let (:contacts) {[
+    {"name"=>"name1", "address"=>"address1"},
+    {"name"=>"name2", "address"=>"address2"},
+    {"name"=>"name3", "address"=>"address3"}
+  ]}
+
+  let(:contents) {'[{"name":"name1","address":"address1"},{"name":"name2","address":"address2"},{"name":"name3","address":"address3"}]'}
+  let (:file)    {instance_double(File).as_null_object}
+  let (:writer)  {described_class.new(file)}
+
+  it "saves contacts to a file" do
+    writer.write_contacts(contacts)
+    expect(file).to have_received(:write).with(contents)
+  end
+
+end
