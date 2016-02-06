cb9ca1ff9d7e89f9236c53d047f382403ee0aa22
me-stevens <me-stevens@users.noreply.github.com>
2016-02-06 18:13:43 +0000

    Adapt the DB to handle contacts with ids.

--- lib/db/db.rb
+++ lib/db/db.rb
@@ -9,26 +9,62 @@ class DB
     all.size
   end
 
-  def add(contact)
-    contacts = all
-    contacts << contact
-    writer.run(contacts) if !writer.nil?
-  end
-
   def all
     reader.nil? ? [] : reader.run
   end
 
-  def at(id)
-    all.empty? ? {} : all[id]
+  def at(index)
+    empty || invalid(index) ? {} : all[index]
+  end
+
+  def add(contact)
+    contacts = all << format(contact)
+    writer.run(contacts) if !writer.nil?
+  end
+
+  def update(contact)
+    contacts = updated_with(format(contact))
+    writer.run(contacts) if !writer.nil?
   end
 
   def search(term)
-    all.select{ |contact| contact.any? {|key, val| val.include?(term)} }
+    all.select { |contact| contact.any? {|key, val| val.to_s.include?(term)} }
+  end
+
+  def index_of_id(id)
+    all.index { |contact| contact["id"] == id }
   end
 
   private
 
   attr_reader :reader, :writer
+
+  def empty
+    all.empty?
+  end
+
+  def invalid(index)
+    index.nil? ? true : all[index].nil?
+  end
+
+  def format(contact)
+    contact.has_key?("id") ? contact : append_id(contact, id_of_new)
+  end
+
+  def append_id(contact, id)
+    contact_with_id       = {}
+    contact_with_id["id"] = id
+    contact_with_id.merge!(contact)
+  end
+
+  def id_of_new
+    size == 0 ? 1 : at(size - 1)["id"] + 1
+  end
+
+  def updated_with(contact)
+    contacts = all
+    index    = index_of_id(contact["id"])
+    invalid(index) ? contacts << contact : contacts[index] = contact
+  end
 
 end
--- spec/db/db_spec.rb
+++ spec/db/db_spec.rb
@@ -2,27 +2,13 @@ require 'db/db'
 
 describe DB do
 
-  let(:reader) {instance_double(Reader).as_null_object}
-  let(:writer) {instance_double(Writer).as_null_object}
-  let(:db)     {described_class.new(reader, writer)}
+  let(:reader)  {instance_double(Reader).as_null_object}
+  let(:writer)  {instance_double(Writer).as_null_object}
+  let(:db)      {described_class.new(reader, writer)}
+  let(:contact) {{"name" => "name", "address" => "address"}}
 
   before :each do
     allow(reader).to receive(:run).and_return([])
-  end
-
-  it "adds one contact to the database" do
-    contact = {"name" => "name", "address" => "address"}
-    db.add(contact)
-    expect(db.size).to eq(1)
-    expect(writer).to have_received(:run).once
-  end
-
-  it "adds two contacts to the database" do
-    contact = {"name" => "name", "address" => "address"}
-    db.add(contact)
-    db.add(contact)
-    expect(db.size).to eq(2)
-    expect(writer).to have_received(:run).twice
   end
 
   it "returns an empty list of contacts if the reader is null" do
@@ -32,9 +18,9 @@ describe DB do
 
   it "sends the list of all the contacts" do
     contacts = [
-      {"name" => "name1", "address" => "address1"},
-      {"name" => "name2", "address" => "address2"},
-      {"name" => "name3", "address" => "address3"}
+      {"id" => 1, "name" => "name1", "address" => "address1"},
+      {"id" => 2, "name" => "name2", "address" => "address2"},
+      {"id" => 3, "name" => "name3", "address" => "address3"}
     ]
     allow(reader).to receive(:run).and_return(contacts)
     expect(db.all).to eq(contacts)
@@ -42,22 +28,75 @@ describe DB do
 
   it "gets an empty contact if the reader is null" do
     db = described_class.new(nil, writer)
-    db.add({"name" => "name", "address" => "address"})
+    db.add(contact)
     expect(db.at(0)).to eq({})
   end
 
-  it "if everything goes well, gets one contact by id" do
-    contact = {"name" => "name", "address" => "address"}
+  it "gets an empty contact if there are no contacts" do
+    expect(db.at(0)).to eq({})
+  end
+
+  it "gets an empty contact if the index is not valid" do
     db.add(contact)
-    allow(reader).to receive(:run).and_return([contact])
-    expect(db.at(0)).to eq(contact)
+    expect(db.size).to eq(1)
+    expect(db.at(5)).to eq({})
+  end
+
+  it "gets one contact if there are contacts and the index is valid" do
+    db.add(contact)
+    contact_with_id = {"id" => 1, "name" => "name", "address" => "address"}
+    expect(db.at(0)).to eq(contact_with_id)
+  end
+
+  it "adds one contact to the database" do
+    db.add(contact)
+    expect(db.size).to eq(1)
+    expect(writer).to have_received(:run).once
+  end
+
+  it "first contact has an id of 1" do
+    db.add(contact)
+    expect(db.at(0)["id"]).to eq(1)
+  end
+
+  it "new contact has an id of previous contact id plus 1" do
+    db.add(contact)
+    db.add(contact)
+    expect(db.at(0)["id"]).to eq(1)
+    expect(db.at(1)["id"]).to eq(2)
+  end
+
+  it "appends a unique id to the contact if it doesn't have one" do
+    contact_with_id = {"id" => 1, "name" => "name", "address" => "address"}
+    db.add(contact)
+    expect(db.at(0)).to eq(contact_with_id)
+  end
+
+  it "doesn't append a unique id to the contact if it already has one" do
+    contact_with_id = {"id" => 1, "name" => "name", "address" => "address"}
+    db.add(contact_with_id)
+    expect(db.at(0)).to eq(contact_with_id)
+  end
+
+  it "finds the index of a contact with the same id as another" do
+    contact_with_id = {"id" => 1, "name" => "name", "address" => "address"}
+    db.add(contact)
+    expect(db.index_of_id(contact_with_id["id"])).to eq(0)
+  end
+
+  it "updates a contact if a contact with the same id is passed" do
+    contact_with_id = {"id" => 1, "name" => "new name", "address" => "new address"}
+    db.update(contact)
+    db.update(contact_with_id)
+    expect(db.size).to eq(1)
+    expect(db.at(0)).to eq(contact_with_id)
   end
 
   it "finds one contact given an exact term" do
     contacts = [
-      {"name" => "name1", "address" => "address1"},
-      {"name" => "name2", "address" => "address2"},
-      {"name" => "name3", "address" => "address3"}
+      {"id" => 1, "name" => "name1", "address" => "address1"},
+      {"id" => 2, "name" => "name2", "address" => "address2"},
+      {"id" => 3, "name" => "name3", "address" => "address3"}
     ]
     allow(reader).to receive(:run).and_return(contacts)
     expect(db.search("address1")).to eq([contacts.first])
@@ -65,9 +104,9 @@ describe DB do
 
   it "finds several contacts given an inexact term" do
     contacts = [
-      {"name" => "name1", "address" => "address1"},
-      {"name" => "name2", "address" => "address2"},
-      {"name" => "name3", "address" => "address3"}
+      {"id" => 1, "name" => "name1", "address" => "address1"},
+      {"id" => 2, "name" => "name2", "address" => "address2"},
+      {"id" => 3, "name" => "name3", "address" => "address3"}
     ]
     allow(reader).to receive(:run).and_return(contacts)
     expect(db.search("address")).to eq(contacts)
