116fa99dad1ff9f224c0cc5cfff481dcff7d7b06
private <user@example.com>
2018-07-24 22:31:09 +0100

    Fix an IO error in the writers
    Both writers were throwing the error:
    
       Failure/Error: file.truncate(0)
    
       IOError:
         not opened for writing
    
    Same on file.write. This was due to the line:
    
        # frozen_string_literal: true
    
    The string contents were frozen and ouldn't be modified.
    StringIO expressed it with the abovementioned IOError.

--- spec/db/writer_spec.rb
+++ spec/db/writer_spec.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 require 'db/writer'
 require 'json'
@@ -13,41 +13,45 @@ RSpec.describe Writer do
   end
 
   let(:contents) do
-    %(
-      [
-        {"name":"name1","address":"address1"},
-        {"name":"name2","address":"address2"},
-        {"name":"name3","address":"address3"}
-      ]
-    )
-  end
-
-  it 'replaces the contents of the file' do
-    file        = StringIO.new(contents)
-    one_contact = [{ 'name' => 'name1', 'address' => 'address1' }]
-    writer      = described_class.new(file)
-    writer.write(one_contact)
-    expect(file.string).to eq('[{"name":"name1","address":"address1"}]')
+    '[' \
+    '{"name":"name1","address":"address1"},' \
+    '{"name":"name2","address":"address2"},' \
+    '{"name":"name3","address":"address3"}' \
+    ']'
   end
 
   it 'saves the contents of the file' do
-    file   = StringIO.new('')
-    writer = described_class.new(file)
-    writer.write(contacts)
+    file = StringIO.new
+    described_class.new(file).write(contacts)
     expect(file.string).to eq(contents)
   end
 
   it 'saves the contents of the file in JSON format' do
-    file   = StringIO.new('')
-    writer = described_class.new(file)
-    writer.write(contacts)
+    file = StringIO.new
+    described_class.new(file).write(contacts)
     expect(file.string).to eq(contacts.to_json)
   end
 
+  it 'replaces the contents of the file' do
+    file = StringIO.new(contents)
+    one_contact = [{ 'name' => 'name1', 'address' => 'address1' }]
+    described_class.new(file).write(one_contact)
+    expect(file.string).to eq('[{"name":"name1","address":"address1"}]')
+  end
+
   it 'flushes the contents to the file' do
-    file   = instance_double(File).as_null_object
-    writer = described_class.new(file)
-    writer.write(contacts)
+    file = instance_double(File).as_null_object
+    described_class.new(file).write(contacts)
     expect(file).to have_received(:flush)
+  end
+
+  xit 'works with a real file' do
+    File.open('test', 'w') { |file| file.write(contents) }
+    file = File.new('test', 'a+')
+    one_contact = [{ 'name' => 'name1', 'address' => 'address1' }]
+    described_class.new(file).write(one_contact)
+    expect(file.read).to eq('[{"name":"name1","address":"address1"}]')
+    file.close
+    File.delete(file)
   end
 end
